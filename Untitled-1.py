003d55dd646da32c82748fde76633716a05148b8
Ash Pierrot <pierrot.youtub@gmail.com>
2024-04-30 16:06:24 +0300

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, classification_report
